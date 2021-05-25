b6f411a06ee7fd65ccc3f20e7bfd881eb10c0d74
Ty Chermsirivatana <professornitro@gmail.com>
2021-05-25 14:48:08 -0400

    Yes

---
+++ leviathan.sh
@@ -0,0 +1,43 @@
+#!/bin/bash 
+# Install Dependencies
+sudo apt-get install figlet -y -qq > /dev/null #Installs for dat style
+sleep 1s 
+clear
+tput setaf 2 # Sets color to Green
+#Enviromental Variables 
+USER_GRAFANA="ADMIN"
+PASSWORD_GRAFANA="=7(SfVJ%wA_B7B{q(3TkLe@x36/q4.s{zb~yGY"
+USER_INFLUX="ADMIN"
+PASSWORD_INFLUX="{k7ws3/#Bv>@BL&<[_HHww?YTqZAsN9gfv#{!L"
+PREFIX="[LEVIATHAN]"
+# Title + Menu
+figlet -w 500  Project Leviathan
+figlet -w 100 by WarpWing
+echo -ne '#####                     (33%)\r'
+sleep 1
+echo -ne '#############             (66%)\r'
+sleep 1
+echo -ne '#######################   (100%)\r'
+echo -ne '\n'
+
+sleep 3s
+clear 
+echo $PREFIX Welcome to Project Leviathan
+echo $PREFIX Are you ready to begin installing? [y/n]: 
+
+
+
+
+
+# Functions 
+
+#Grafana Install Function
+#function Grafana {
+                        
+                
+
+
+#InfluxDB Install Function
+#function InfluxDB {
+                        
+                
