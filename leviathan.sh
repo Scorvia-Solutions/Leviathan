e2fd331b28899592be5082f72951e232f80cc3bd
Ty Chermsirivatana <professornitro@gmail.com>
2021-05-25 19:07:31 -0400

    Install Part done

--- leviathan.sh
+++ leviathan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 # Install Dependencies
-sudo apt-get install figlet -y -qq > /dev/null #Installs for dat style
+sudo apt-get install figlet -y -qq &> /dev/null #Installs for dat style
 sleep 1s 
 clear
 tput setaf 2 # Sets color to Green
@@ -13,12 +13,45 @@ PREFIX="[LEVIATHAN]"
 # Functions 
 
 #Grafana Install Function
-#function Grafana {
-                        
+function Grafana () {
+    echo $PREFIX Downloading Grafana Dependencies
+    sudo apt install -y software-properties-common wget apt-transport-https &> /dev/null
+    echo $PREFIX Retrieving and Adding Grafana GPG Key
+    wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add - &> /dev/null
+    echo $PREFIX Adding Stable Repository to Source List
+    echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list &> /dev/null
+    echo $PREFIX Updating Source List
+    sudo apt update -y  &> /dev/null
+    echo $PREFIX Installing Grafana 
+    sudo apt install -y grafana &> /dev/null
+    echo $PREFIX Optimizing Grafana 
+    sleep 4s #This sleep is to wait for Grafana to setup!
+    echo $PREFIX Grafana Installation Complete. Moving on to InfluxDB!
+    sleep 2s
+
+
+}
                 
 
 #InfluxDB Install Function
-#function InfluxDB {
+function InfluxDB () { 
+    echo $PREFIX Retrieving InfluxDB GPG Key and Adding it
+    curl -s https://repos.influxdata.com/influxdb.key | sudo apt-key add - &> /dev/null
+    echo $PREFIX Recovering Distrib Information
+    source /etc/lsb-release
+    echo $PREFIX Adding Stable Repository to Source List 
+    echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list &> /dev/null
+    echo $PREFIX Updating Source List 
+    sudo apt update -y 
+    echo $PREFIX Installing InfluxDB 
+    sudo apt install -y influxdb 
+    echo $PREFIX Unmasking,Enabling and Starting InfluxDB 
+    sudo systemctl unmask influxdb.service &> /dev/null
+    sudo systemctl enable influxdb &> /dev/null
+    sudo systemctl start influxdb &> /dev/null
+    #echo $PREFIX Optimizing InfluxDB
+    
+} 
 
 
 
@@ -29,9 +62,19 @@ figlet -w 100 by WarpWing
 sleep 3s
 clear 
 echo $PREFIX Welcome to Project Leviathan
-echo $PREFIX Are you ready to begin installing? [y/n]: 
-
-
+read -p "$PREFIX Are you ready to begin installing? [y/n]: " INPUT
+if [[ $INPUT == y ]]
+then
+  Grafana
+  InfluxDB
+  echo $PREFIX Automatic Install Complete! Make sure to check that everything is properly configured!
+  echo $PREFIX Please remember that the default user and password for Grafana is $USER_GRAFANA : $PASSWORD_GRAFANA
+  echo $PREFIX Also not that the default user and password for InfluxDB is $USER_INFLUX : $PASSWORD_INFLUX
+  echo $PREFIX Project Leviathan Install Complete. Session Terminating in 3 seconds
+  sleep 3s
+else
+  echo Terminating Installation
+fi
 
 
 
