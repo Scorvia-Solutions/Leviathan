1b0143acb00a95d7e0e02b3f2853716411a870c4
Ty Chermsirivatana <professornitro@gmail.com>
2021-05-25 15:04:48 -0400

    Rearrange function positioning

--- leviathan.sh
+++ leviathan.sh
@@ -10,6 +10,19 @@ PASSWORD_GRAFANA="=7(SfVJ%wA_B7B{q(3TkLe@x36/q4.s{zb~yGY"
 USER_INFLUX="ADMIN"
 PASSWORD_INFLUX="{k7ws3/#Bv>@BL&<[_HHww?YTqZAsN9gfv#{!L"
 PREFIX="[LEVIATHAN]"
+# Functions 
+
+#Grafana Install Function
+#function Grafana {
+                        
+                
+
+#InfluxDB Install Function
+#function InfluxDB {
+
+
+
+
 # Title + Menu
 figlet -w 500  Project Leviathan
 figlet -w 100 by WarpWing
@@ -22,15 +35,6 @@ echo $PREFIX Are you ready to begin installing? [y/n]:
 
 
 
-# Functions 
 
-#Grafana Install Function
-#function Grafana {
-                        
-                
-
-
-#InfluxDB Install Function
-#function InfluxDB {
                         
                 
